357e0a0f5a1fb3727b961d6f1a69e442e2741a60
evapopescu22 <evapopescu22@yahoo.com>
2020-12-09 22:26:11 +0200

    Fisier json

--- BestPrice.py
+++ BestPrice.py
@@ -1,6 +1,8 @@
 import requests
+import json
+import re
 from bs4 import BeautifulSoup
-
+product_iformations = []
 def trade_spider(urls):
  for url in urls:
     source_code = requests.get(url)
@@ -15,12 +17,7 @@ def trade_spider(urls):
         best_price = div.string
         if(count == 1):
          print(best_price)
-    most_expensive(url)
-    for span in soup.findAll('span', {'class': 'product-num visible-xs-inline-block visible-sm-inline-block'}):
-        for span2 in span.find_all('span'):
-         print(span2.string)
-
-def most_expensive(url):
+         break
     new_url = url + "?orderby=2"
     source_code = requests.get(new_url)
     plain_text = source_code.text
@@ -31,6 +28,18 @@ def most_expensive(url):
         most_expensive = div.string
         if (count == 1):
             print(most_expensive)
+            break
+    for span in soup.findAll('span', {'class': 'product-num visible-xs-inline-block visible-sm-inline-block'}):
+        for span2 in span.find_all('span'):
+            number = span2.string
+            number = re.split('\s', number)[0]
+            print(number)
+    product_iformations.append({"name": name.strip(),
+                                "best price": best_price,
+                                "most expensive": most_expensive,
+                                "number of products":int(number)})
+    with open("products_json_detail.json", "w") as prod:
+        json.dump(product_iformations, prod)
 
 urls = ['https://www.compari.ro/monitoare-c3126/', 'https://incarcator-auto.compari.ro/', 'https://www.compari.ro/masini-de-spalat-c3167/']
 trade_spider(urls)
